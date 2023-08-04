5f832ffecd046a19bd3753f6f857045b01289241
vanessa.fagundes <vanessa.fagundes@ifood.com.br>
2023-08-04 10:37:20 -0300

    Initial Commit

---
+++ PriceCheck/PriceCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriceCheckApp.swift
+//  PriceCheck
+//
+//  Created by Vanessa Fagundes on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PriceCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
